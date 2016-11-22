adc07cdc8f8d906a8f63a0ec39703eee118ae730
Stefan Mermas <stefan_mermans@hotmail.com>
2016-11-22 13:10:13 +0100

    stuff

--- CsharpHueAssignment/CsharpHueAssignment/MainPage.xaml.cs
+++ CsharpHueAssignment/CsharpHueAssignment/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
